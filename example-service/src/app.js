e663f526d729ddf15a83453aa1f030f3138e9d8e
TreyH <vbhayden3@gmail.com>
2019-04-12 10:54:40 -0400

    adding example service

---
+++ example-service/src/app.js
@@ -0,0 +1,131 @@
+//
+// This is an example NodeJS Express application using Keycloak
+//
+const fs = require("fs");
+const cors = require("cors");
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require("body-parser");
+const Keycloak = require('keycloak-connect');
+
+const orientjs = require("./lib/db");
+
+// Read our Keycloak config file
+const keycloakConfig = JSON.parse(fs.readFileSync("./keycloak.json"));
+
+// Adjust for our docker env variable
+keycloakConfig["auth-server-url"] = "http://192.168.30.162:8081/auth"
+
+// Constants
+const PORT = (process.env.port || 3000);
+
+// Create an instance of the express class and declare our port
+const app = express();
+const memory = new session.MemoryStore();
+const keycloak = new Keycloak({store: memory}, keycloakConfig);
+
+// Set up our session        
+app.use(
+    session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: true,
+        store: memory
+    })
+);
+
+// Set EJS as our view engine for partial templates
+app.set("view engine", "ejs");
+app.set('json spaces', 2);
+
+// Use our keycloak middleware
+app.use(keycloak.middleware());
+
+// Boiletplate middleware
+app.use(bodyParser.json())
+app.use(cors())
+
+// Get our static files.  We're keeping these in a few different places, so this
+// might get sort of confusing when looking directly at the href paths
+app.use(express.static("public"));
+app.use(express.static("views"));
+
+// Set our renderer and configurations
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(function(req, res, next) {
+
+    // Use a callback to check whether our user's token has the given
+    // roles required to access parts of the site.
+    //
+    function confirmRoles(token, request) {
+
+        // At the time of this callback, we'll already have a token from
+        // the Keycloak user.  The role confirmation will just be here
+        // to append that role verification to the response variable.
+        //
+        res.locals.admin = token.hasRole("realm:admin");
+
+        // False here will force a hideous redirect to the Keycloak "permission denied"
+        // page, so we'll want to handle that more gracefully.
+        return true;
+    }
+
+    // Unpack the middleware
+    let protect = keycloak.protect(confirmRoles);
+
+    protect(req, res, next);
+})
+
+// // Redirecting someone to logout once their state gets weird
+// keycloak.accessDenied = function(req, res, next) {
+//     res.redirect("/logout")
+// }
+
+// // Get our user information
+// app.use(function(req, res, next){
+    
+//     // This isn't really documented, but we can use the access token to get our user info
+//     if (req.kauth && req.kauth.grant && req.kauth.grant.access_token) {
+//         let content = req.kauth.grant.access_token.content;
+
+//         res.locals.id = content.sub;
+//         res.locals.username = content.preferred_username;
+
+//         console.log(req.kauth.grant.access_token.content)
+//     }
+//     next();
+// });
+
+app.get("/aka", function(res, req, next){
+    res.render("index", {
+        admin: res.locals.admin,
+        id: res.locals.id,
+        user: res.locals.username
+    });
+});
+
+app.get("/aka/:realm/:id", function(req, res, next){
+    
+    res.render("index", {
+        admin: res.locals.admin,
+        id: res.locals.id,
+        user: res.locals.username
+    });
+});
+app.get("*", function(req, res, next){
+    res.redirect("/aka");
+});
+
+// There's a problem if this is included earlier, not sure why
+app.use(keycloak.middleware({
+    logout: '/'
+}));
+
+// Start the service
+app.listen(PORT, function () {
+    console.log(`\n[Server] listening on port ${PORT} ...`)
+});
---
+++ example-service/src/lib/db.js
@@ -0,0 +1,127 @@
+const orientjs = require("orientjs");
+
+const DB_NAME = "FIM";
+const CLASS_ALIAS_NAME = "User";
+const CLASS_REALM_NAME = "Realm";
+
+const server = orientjs({
+    host: (process.env.ORIENT_HOST || "192.168.30.162"),
+    port: (process.env.ORIENT_PORT || "2424"),
+    username: (process.env.ORIENT_USER || "root"),
+    password: (process.env.ORIENT_PASS || "root"),
+});
+
+/** @type {orientjs.ODatabase} */
+var db = undefined;
+
+function initDatabase(dbs) {
+   for (let db_ of dbs) {
+        if (db_.name == DB_NAME) {
+            console.log("[Orient] Found Existing Orient Database:", db_.name)
+            initClasses(db = db_);
+            return;
+        }
+    }
+    if (db == undefined) {
+        server.create({
+            name: DB_NAME,
+            type: 'graph',
+            storage: 'plocal'
+        }).then(created => {
+            console.log("[Orient] Created New Orient Database:", created.name)
+            initClasses(created);
+        })
+    }
+}
+
+/**
+ * 
+ * @param {orientjs.ODatabase} database 
+ */
+function initClasses(database) {
+    database.class.list().then(classes => {
+        ensureClass(classes, CLASS_ALIAS_NAME, [
+            { name: "id", type: "String"},
+            { name: "name", type: "String"},
+        ]);
+        ensureClass(classes, CLASS_REALM_NAME, [
+            { name: "id", type: "String"},
+            { name: "name", type: "String"},
+        ]);
+    })
+}
+
+function ensureClass(classes, name, props) {
+    for (let c of classes) {
+        if (c.name == name) {
+            console.log("[Orient] Found Existing Class:", name)
+            return;
+        }
+    }
+    db.class.create(name, "V").then(c => {
+        console.log("[Orient] Created New Class:", c.name);
+        c.property.create(props).then(p => {
+            console.log(`[Orient] Created Properties for ${c.name}:`, p);
+        });
+    });
+}
+
+// Get our DB from Orient
+server.list().then(initDatabase);
+
+module.exports = {
+    
+    /**
+     * Callback returning a list of all realms available to the FIM DB.
+     *
+     * @callback realmReturnCallback
+     */
+    /**
+     * Retrieves every known alias for the given user ID / realm pair.
+     * @param {realmReturnCallback} cb 
+     */
+    getRealms: function(cb) {
+        db.query()
+    },
+
+    /**
+     * Callback returning a master ID.
+     *
+     * @callback idReturnCallback
+     * @param {string} id - The ID representing the user in your master FIM system.
+     */
+    /**
+     * Retrieves the master alias for the given user ID / realm pair.
+     * @param {string} id 
+     * @param {string} realm 
+     * @param {idReturnCallback} callback 
+     */
+    getMasterAlias: function(id, realm, callback) {
+        
+        let query = `
+            select id from (
+                select expand(out("AliasFor")) from (
+                    select expand(in("BelongsTo")) from Realm where id = "${db.escape(realm)}"
+                ) where id = "${db.escape(id)}"
+            )
+        `
+        db.query(query).then(results => {
+            if (results.length == 0)
+                callback(null);
+            else if (results.length == 1)
+                callback(results[0].id);
+            else {
+
+                // This is problematic, but won't be if these are all the same...
+                let id_ = results[0].id;
+                for (let result in results) {
+                    if (id_ != result.id) {
+                        console.warn("[Orient] IMPROPER TABLE CONFIGURATION, MULTIPLE IDs FOR SAME USER ON SAME REALM");
+                        break;
+                    }
+                }
+                callback(id_);
+            }
+        });
+    },
+}
